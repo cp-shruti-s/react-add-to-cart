82cba22343dde8c107afeb6d2fd95a3a8fb9adfa
cp-shruti-s <shruti.s@canopas.com>
2022-03-14 03:44:43 +0530

    Implement Basic setup for add to card items

---
+++ src/data.js
@@ -0,0 +1,30 @@
+import grapes from './img/grapes.jpg';
+import banana from './img/banana.jpg';
+import apple from './img/apple.jpg';
+
+const data = {
+    productData: [
+        {
+            id: 1,
+            img: grapes,
+            title: 'Juicy Grapes',
+            desc: '',
+            price: 100,
+        },
+        {
+            id: 2,
+            img: banana,
+            title: 'Sweet Banana',
+            desc: '',
+            price: 30,
+        }, {
+            id: 3,
+            img: apple,
+            title: 'Red & Green Apple',
+            desc: '',
+            price: 150,
+        }
+    ],
+};
+
+export default data;
